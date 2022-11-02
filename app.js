c24cedb05fcb4fb75b1385e412afa5fd5ddcb3e5
Yash Kaskar <yash.kaskar99@gmail.com>
2022-11-02 11:37:58 +0530

    adding math operational

--- app.js
+++ app.js
@@ -1,54 +1,68 @@
-const display1el = document.querySelector('.display-1');
-const display2el = document.querySelector('.display-2');
-const tempresultel = document.querySelector('.temresult');
-
-const numbersel = document.querySelectorAll('.number');
-const operationel = document.querySelectorAll('.operation');
-const equalel = document.querySelector('.equal');
-const clearel = document.querySelector('.allclear');
-const cleareel = document.querySelector('.alllastclear');
-
-// Variables :
-let display1Num = '';
-let display2Num = '';
+const display1El = document.querySelector(".display-1");
+const display2El = document.querySelector(".display-2");
+const tempResultEl = document.querySelector(".temresult");
+const numbersEl = document.querySelectorAll(".number");
+const operationEl = document.querySelectorAll(".operation");
+const equalEl = document.querySelector(".equal");
+const clearAllEl = document.querySelector(".all-clear");
+const clearLastEl = document.querySelector(".last-entity-clear");
+let dis1Num = "";
+let dis2Num = "";
 let result = null;
-let lastOperation = '';
-let dot = false;
+let lastOperation = "";
+let haveDot = false;
 
+numbersEl.forEach((number) => {
+  number.addEventListener("click", (e) => {
+    if (e.target.innerText === "." && !haveDot) {
+      haveDot = true;
+    } else if (e.target.innerText === "." && haveDot) {
+      return;
+    }
+    dis2Num += e.target.innerText;
+    display2El.innerText = dis2Num;
+  });
+});
 
-// display2 numbers and dot
-numbersel.forEach( number => {
-    number.addEventListener('click', (e) => {    
-        if(e.target.innerText === '.' && !dot){
-            dot = true;
-        } else if (e.target.innerText === '.' && dot) { 
-            return;
-        }
-        display2Num += e.target.innerText;
-        display2el.innerText = display2Num;
-    })
-})
+// operational function
 
-// mathematical operation
-operationel.forEach(operation => {
-    operation.addEventListener('click', (e) => {    
-        if (!display2Num) return;
-        dot = false;
-        const operationName = e.target.innerText;
-        if (display1Num && display2Num && lastOperation) {   
-            mathOperation();   
-        } else {      
-            result = parseFloat(display2Num); 
-        }
-        clearVar(operationName);
-        console.log(result);
-        })
-})
+operationEl.forEach((operation) => {
+  operation.addEventListener("click", (e) => {
+    if (!dis2Num) return;
+    haveDot = false;
+    const operationName = e.target.innerText;
+    if (dis1Num && dis2Num && lastOperation) {
+      mathOperation();
+    } else {
+      result = parseFloat(dis2Num);
+    }
+    clearVar(operationName);
+    lastOperation = operationName;
+    console.log(result);
+  });
+});
 
-function clearVar(name = '') {   
-    display1Num += display2Num + '' + name + '';
-    display1el.innerText = display1Num;
-    display2el.innerText = '';
-    display2Num = '';
-    tempresultel.innerText = result;
+// clear dis2Num
+function clearVar(name = "") {
+  dis1Num += dis2Num + " " + name + " ";
+  display1El.innerText = dis1Num;
+  display2El.innerText = "";
+  dis2Num = "";
+  tempResultEl.innerText = result;
+}
+
+// math operational
+
+function mathOperation() {
+  if (lastOperation === "x") {
+    result = parseFloat(result) * parseFloat(dis2Num);
+  } else if (lastOperation === "+") {
+    result = parseFloat(result) + parseFloat(dis2Num);
+  } else if (lastOperation === "-") {
+    result = parseFloat(result) - parseFloat(dis2Num);
+  } else if (lastOperation === "/") {
+    result = parseFloat(result) / parseFloat(dis2Num);
+  } else if (lastOperation === "%") {
+    result = parseFloat(result) % parseFloat(dis2Num);
+  }
 }
